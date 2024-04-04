d55441839020c43364b0d61cb9000ae1f1a75c79
JulsVilor <julsviloria@gmail.com>
2024-04-04 14:36:33 +0800

    a

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -13,13 +13,40 @@ export const DashboardPage = () => {
       <h1>DashboardPage</h1>
       {isToken}
 
-      <div className="ui placeholder segment">
-        <div className="ui icon header">
-          Welcome! {getUser["rolename"]}: {getUser["nickname"]}
+      <div className="ui message">
+        <i className="close icon"></i>
+        <div className="header">
+          Welcome back {getUser["rolename"]}: {getUser["nickname"]}!
         </div>
-        <div className="inline">
-          <div className="ui text button teal">
-            
+        <p>You have some work to do!</p>
+      </div>
+
+      <h5 className="ui top attached header">
+        Tasks
+      </h5>
+      
+      <div className="ui attached segment">
+        <div className="ui relaxed divided list">
+          <div className="item">
+            <i className="check circle outline middle aligned icon"></i>
+            <div className="content">
+              <a className="header">Requesting Account Approval</a>
+              <div className="description">ID: 9330898</div>
+            </div>
+          </div>
+          <div className="item">
+            <i className="check circle middle aligned icon"></i>
+            <div className="content">
+              <a className="header">Approved Account Request</a>
+              <div className="description">ID: 940898</div>
+            </div>
+          </div>
+          <div className="item">
+            <i className="times circle middle aligned icon"></i>
+            <div className="content">
+              <a className="header">Denied Account Request</a>
+              <div className="description">ID: 45940898</div>
+            </div>
           </div>
         </div>
       </div>
