b6f2986dc76f56fac59aa3409dbd02279ae4d47f
JulsVilor <julsviloria@gmail.com>
2024-04-09 10:54:40 +0800

    a

--- src/utilities/upsert/applications.jsx
+++ src/utilities/upsert/applications.jsx
@@ -1,10 +1,49 @@
 
 export const UpsertApplication = () => {
 
-    return (
-      <div className="expand-centered">
-        <h1>UpsertApplication</h1>
 
+
+    return (
+      <div className="ui segment">
+        <h1>Insert / Update Applications</h1>
+
+        <div className="ui form">
+
+          <div className="field">
+            <label>ID</label>
+            <input type="text" />
+          </div>
+
+          <div className="field">
+            <label>Name</label>
+            <input type="text" />
+          </div>
+
+          <div className="field">
+            <label>Company</label>
+            <input type="text" />
+          </div>
+
+          <div className="field">
+            <label>Details</label>
+            <input type="text" />
+          </div>
+
+          <div className="field">
+            <label>Image</label>
+            <input type="text" />
+          </div>
+
+          <div className="field">
+            <label>Status</label>
+            <input type="text" />
+          </div>
+
+          <div className="field">
+            <div className="ui button purple">Submit Application</div>
+          </div>
+
+        </div>
       </div>
     );
   };
