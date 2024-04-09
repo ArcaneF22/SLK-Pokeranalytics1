1707f03f0f89e133f61d4706ae5754bb2f55f152
JulsVilor <julsviloria@gmail.com>
2024-04-09 21:49:49 +0800

    a

--- src/pages/history.jsx
+++ src/pages/history.jsx
@@ -4,7 +4,7 @@ export const HistoryPage = () => {
 
     return (
       <div className="expand-centered">
-        <h1>HistoryPage</h1>
+        <h1>History Pages</h1>
         <FetchHistory />
       </div>
     );
