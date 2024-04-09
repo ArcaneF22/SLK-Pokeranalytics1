63aa22075bfa4225bd7471a6dd417acbf5e4836a
JulsVilor <julsviloria@gmail.com>
2024-04-09 10:51:10 +0800

    a

--- src/pages/applications.jsx
+++ src/pages/applications.jsx
@@ -1,10 +1,12 @@
 import { FetchApplications } from '../utilities/fetch/applications'
+import { UpsertApplications } from '../utilities/upsert/applications'
 
 export const ApplicationsPage = () => {
 
     return (
       <div className="expand-centered">
         <h1>ApplicationsPage</h1>
+        <UpsertApplications />
         <FetchApplications />
       </div>
     );
---
+++ src/utilities/upsert/applications.jsx
@@ -0,0 +1,11 @@
+
+export const UpsertApplication = () => {
+
+    return (
+      <div className="expand-centered">
+        <h1>UpsertApplication</h1>
+
+      </div>
+    );
+  };
+  
