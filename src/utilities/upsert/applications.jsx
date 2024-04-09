2860d0d61d6624bf20a5e4b2cf90835f44d17df1
JulsVilor <julsviloria@gmail.com>
2024-04-09 11:08:48 +0800

    aa

--- src/utilities/upsert/applications.jsx
+++ src/utilities/upsert/applications.jsx
@@ -2,12 +2,34 @@ import { useState } from "react";
 
 export const UpsertApplications = () => {
 
-  const appID =       useState("")
-  const appName =     useState("")
-  const appCompany =  useState("")
-  const appDetails =  useState("")
-  const appImage =    useState("")
-  const appStatus =   useState("")
+  const [loading, setLoading] =         useState(false);
+  const [message, setMessage] =         useState("");
+
+  const [appID, setappID] =             useState("");
+  const [appName, setappName] =         useState("");
+  const [appCompany, setappCompany] =   useState("");
+  const [appDetails, setappDetails] =   useState("");
+  const [appImage, setappImage] =       useState("");
+  const [appStatus, setappStatus] =     useState("");
+
+  const validate = (e) => {
+    e.preventDefault()
+    setLoading(true)
+
+
+      if(appName == "" || appCompany == "" || appDetails == "" || appImage == ""){
+            setMessage("Details incomplete!")
+      } else {
+        proceedUpsert()
+      }
+
+
+  }
+
+  const proceedUpsert = async () => {
+    alert("aaa")
+  }
+
 
 
     return (
@@ -18,36 +40,37 @@ export const UpsertApplications = () => {
 
           <div className="field">
             <label>ID</label>
-            <input type="text" />
+            <input type="text" value={appID} onChange={(e) => setappID(e.currentTarget.value)}/>
           </div>
 
           <div className="field">
             <label>Name</label>
-            <input type="text" />
+            <input type="text" value={appName} onChange={(e) => setappName(e.currentTarget.value)}/>
           </div>
 
           <div className="field">
             <label>Company</label>
-            <input type="text" />
+            <input type="text" value={appCompany} onChange={(e) => setappCompany(e.currentTarget.value)}/>
           </div>
 
           <div className="field">
             <label>Details</label>
-            <input type="text" />
+            <input type="text" value={appDetails} onChange={(e) => setappDetails(e.currentTarget.value)}/>
           </div>
 
           <div className="field">
             <label>Image</label>
-            <input type="text" />
+            <input type="text" value={appImage} onChange={(e) => setappImage(e.currentTarget.value)}/>
           </div>
 
           <div className="field">
             <label>Status</label>
-            <input type="text" />
+            <input type="text" value={appStatus} onChange={(e) => setappStatus(e.currentTarget.value)}/>
           </div>
 
           <div className="field">
             <div className="ui button purple">Submit Application</div>
+            <p>{message}</p>
           </div>
 
         </div>
