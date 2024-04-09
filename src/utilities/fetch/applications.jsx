9f0e61ced0e0e5f8d4bfd34b9334e3d423560507
JulsVilor <julsviloria@gmail.com>
2024-04-09 18:36:11 +0800

    aa

--- src/utilities/fetch/applications.jsx
+++ src/utilities/fetch/applications.jsx
@@ -63,7 +63,7 @@ export const FetchApplications = () => {
   const editApplication = (id,name,image,company,details,status) => {
     console.log("Clickedz on button in row with id:", id, name, image, company, details, status);
     
-    sessionStorage.setItem('editApp', [{id: id, name: name, image: image, company: company, details: details, status: status}]);
+    sessionStorage.setItem('editApp', [{"id": id, "name": name, "image": image, "company": company, "details": details, "status": status}]);
   };
 
   return (
