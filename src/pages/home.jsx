8cf135d6daec081df98d5848502486cf4346903f
JulsVilor <julsviloria@gmail.com>
2024-04-18 09:31:26 +0800

    a

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -25,53 +25,14 @@ export const HomePage = () => {
               </div>
           </h2>
       </div>
-      
-    <div className="">
-      
-
-
-      <div className='ui segment middle aligned stackable grid container'>
-        <div className="row">
-            <div className="sixteen wide wide column">
-              <h3>Welcome back {getUser["rolename"]}: {getUser["nickname"]}!</h3>
-              <p>You have some work to do! </p>
-            </div>
-            <div className="sixteen wide wide column">
-              <h3>Welcome back {getUser["rolename"]}: {getUser["nickname"]}!</h3>
-              <p>You have some work to do! </p>
-            </div>
-            <div className="sixteen wide wide column">
-              <h3>Welcome back {getUser["rolename"]}: {getUser["nickname"]}!</h3>
-              <p>You have some work to do! </p>
-            </div>
-        </div>
-        <div className='row'>
-
-        </div>
-      </div>
-      <div class="ui vertical stripe quote segment">
-    <div class="ui equal width stackable internally celled grid">
-      <div class="center aligned row">
-        <div class="column">
-          <h3>"What a Company"</h3>
-          <p>That is what they all say about us</p>
-        </div>
-        <div class="column">
-          <h3>"I shouldn't have gone with their competitor."</h3>
-          <p>
-           <b>Nan</b> Chief Fun Officer Acme Toys
-          </p>
-        </div>
-      </div>
-    </div>
-  </div>
+    
 
       <div className="ui vertical stripe quote segment">
         <div className="ui middle aligned stackable grid container">
           <div className="row">
             <div className="six wide right floated column">
               <h3>Welcome back {getUser["rolename"]}: {getUser["nickname"]}!</h3>
-              <p>You have some work to do! </p>
+
             </div>
             <div className="eight wide left floated column">
               <FetchNotificationPending />
@@ -80,7 +41,7 @@ export const HomePage = () => {
         </div>
       </div>
 
-    </div>
+  
 
 </>
 
